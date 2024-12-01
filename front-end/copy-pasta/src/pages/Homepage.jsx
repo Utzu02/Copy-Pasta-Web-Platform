53e1cd217b4ad3982750ca932fef8312a66dd451
Utzu02 <107960417+Utzu02@users.noreply.github.com>
2024-12-01 04:31:39 +0200

    Configurare rute pagina + adaugare componenta navBar

---
+++ front-end/copy-pasta/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Homepage = () => {
+    return (
+          <>
+          </>
+      )
+}
+export default Homepage
---
+++ front-end/copy-pasta/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    return (
+          <>
+          </>
+      )
+}
+export default Login
